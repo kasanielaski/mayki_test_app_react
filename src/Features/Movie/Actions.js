e6f916813cfc3451723f50bd7426cda4e78b6fdf
Sergey Shapirenko <sshapirenko@gmail.com>
2019-03-11 21:02:02 +0700

    hotfix del action

--- src/Features/Movie/Actions.js
+++ src/Features/Movie/Actions.js
@@ -1,10 +1,9 @@
 import { createActions } from 'redux-actions';
-import { MOVIE__GET, MOVIE__FETCH, MOVIE_FAIL } from './ActionTypes';
+import { MOVIE__GET, MOVIE__FETCH } from './ActionTypes';
 
 const movieActions = createActions({
     [MOVIE__GET]: payload => payload,
-    [MOVIE__FETCH]: () => {},
-    [MOVIE_FAIL]: payload => payload
+    [MOVIE__FETCH]: () => {}
 });
 
 export default movieActions;
--- src/Features/MovieList/Actions.js
+++ src/Features/MovieList/Actions.js
@@ -1,14 +1,9 @@
 import { createActions } from 'redux-actions';
-import {
-    MOVIE_LIST__GET,
-    MOVIE_LIST__FETCH,
-    MOVIE_LIST__FAIL
-} from './ActionTypes';
+import { MOVIE_LIST__GET, MOVIE_LIST__FETCH } from './ActionTypes';
 
 const movieListActions = createActions({
     [MOVIE_LIST__GET]: payload => payload,
-    [MOVIE_LIST__FETCH]: () => {},
-    [MOVIE_LIST__FAIL]: payload => payload
+    [MOVIE_LIST__FETCH]: () => {}
 });
 
 export default movieListActions;
